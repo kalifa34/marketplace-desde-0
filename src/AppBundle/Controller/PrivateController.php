076f5ee9e23e4707f35f133599446a4352c4c7f0
Jose Luis <jlrueda1974@gmail.com>
2016-09-06 22:01:14 +0000

    7.3 Creacion de nuevoTrayecto

---
+++ src/AppBundle/Controller/PrivateController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class PrivateController extends Controller
+{
+    /**
+     * @Route("/nuevoTrayecto", name="private_nuevoTrayecto")
+     */
+    public function nuevoTrayectoAction(Request $request)
+    {
+        
+        /**
+         * muestra un formulario para crear un nuevp trayecto
+         * 
+         * 1.- Crear carpeta nueva /app/Resources/views/trayecto
+         * 1. Habría que copiar los twig de nuevoTrayecto.html.twig
+         * 2. Y indicar en el render que se muestren
+         *  
+         * 
+         * */
+        return $this->render('nuevoTrayecto/index.html.twig');
+    }
+        
+        
+        
+     /**
+     * @Route("/publicarTrayecto", name="private_publicarTrayecto")
+     */
+     
+    public function publicarTrayectoAction(Request $request)
+    {
+    
+        /**
+         * Guarda los datos enviados por el formulario nuevoTrayecto
+         * 
+         * 1. Habría que guardar los datos recibiendos en $_GET en un nuevoTrayecto
+         * 1.- Crear carpeta nueva /app/Resources/views/trayecto
+         * 
+         * */
+        return $this->render('building/index.html.twig');
+    }
+}
+
